2d7b1a4f7b9797cf192a1b1752741e3b1a8b3117
Chris-medi <ccfranm17@gmail.com>
2021-03-26 14:13:01 -0500

    add page with style

---
+++ server/src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import path from "path";
+import { Upload } from "./Middlewares/Upload";
+
+// Use only in development mode.
+import morgan from "morgan";
+
+const app = express();
+
+// Allow origins cors
+app.use(cors());
+
+// Set more security on requests
+app.use(helmet());
+
+// Help with routes information
+app.use(morgan("combined"));
+
+// Set default storage
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Allow and parse json objects
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+export default app;
