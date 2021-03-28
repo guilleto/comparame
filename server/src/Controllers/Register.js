5426bfa39437afaf3e18ce34409e2aa011ea22eb
Guille <guilleccubillos@hotmail.com>
2021-03-28 16:31:11 -0500

    Register Controller - Errace Services

--- server/src/Controllers/Register.js
+++ server/src/Controllers/Register.js
@@ -1,22 +1,46 @@
-'use strict'
-
 const mongoose = require('mongoose')
 const User = require('../Models/User')
+const jwt = require("jsonwebtoken");
 
-function Register (req,res){
-    const user = new User({
-        rols: req.body.Rol,
-        username: req.body.Username,
-        email: req.body.Email,
-        name: req.body.Name,
-        lastname: req.body.Lastname        
-    })
-
-    user.save((err) => {
-        if (err) return res.status(500).send({
-             message: `Error al crear el usuario: ${err}` 
-            })
-    
-        return res.status(201).send({ token: service.createToken(user) })
-      })
-}
+exports.user_signup = (req, res, next) => {
+    User.find({ email: req.body.email })
+      .exec()
+      .then(user => {
+        if (user.length >= 6) {
+          return res.status(409).json({
+            message: "New Register"
+          });
+        } else {
+          bcrypt.hash(req.body.password, 10, (err, hash) => {
+            if (err) {
+              return res.status(500).json({
+                error: err
+              });
+            } else {
+              const user = new User({
+                rols: req.body.Rol,
+                username: req.body.Username,
+                email: req.body.Email,
+                name: req.body.Name,
+                lastname: req.body.Lastname,
+                password: hash   
+              });
+              user
+                .save()
+                .then(result => {
+                  console.log(result);
+                  res.status(201).json({
+                    message: "User created"
+                  });
+                })
+                .catch(err => {
+                  console.log(err);
+                  res.status(500).json({
+                    error: err
+                  });
+                });
+            }
+          });
+        }
+      });
+  };
