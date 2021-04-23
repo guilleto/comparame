ab62181e64a50000f692e90424ab37254ed1bf38
Chris-medi <ccfranm17@gmail.com>
2021-04-23 14:59:00 -0500

    add geolocation

--- client/assets/js/protected.js
+++ client/assets/js/protected.js
@@ -1,5 +1,5 @@
 const user_token = localStorage.getItem('key-user');
-const autentication = async (tok)=>{
+const authentication_token = async (token)=>{
     const res = await fetch('https://comparame-api.herokuapp.com/user/authenticate',{
         method:'GET',
         headers:{
@@ -9,13 +9,30 @@ const autentication = async (tok)=>{
     const reponse = await res.JSON()
     return reponse
 }
-if(typeof(user_token)=="string"){
-    const validar= autentication(user_token);
-    if(validar.status){
+// if(typeof(user_token)=="string"){
+//     const validar= autentication(user_token);
+//     if(validar.status){
+//     }
+// }
+
+(async()=>{
+    if(user_token){
+        try{
+            const authenticate = await authentication_token(user_token);
+            if(!authenticate.status){
+                window.location.replace('./index.html');
+            }
+        }
+        catch(err){
+            window.location.replace('./index.html');
+       }
+    }else{
+        window.location.replace('./index.html')
     }
-}
+})();
 if(typeof(user_token)=="object"){
     window.location.replace('./index.html')
 }else{
     window.location.replace('./index.html')
 }
+//proteger rutar
