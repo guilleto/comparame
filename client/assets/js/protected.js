9b32f0ae270c0919ea5bde8c964b7adefb9e81c0
Chris-medi <ccfranm17@gmail.com>
2021-05-14 20:09:59 -0500

    agregar pagina para CRUD productos

--- client/assets/js/protected.js
+++ client/assets/js/protected.js
@@ -1,38 +1,50 @@
 const user_token = localStorage.getItem('key-user');
-const authentication_token = async (token)=>{
-    const res = await fetch('https://comparame-api.herokuapp.com/user/authenticate',{
-        method:'GET',
-        headers:{
-            'Authorization' : user_token 
-        }
-    })
-    const reponse = await res.JSON()
-    return reponse
-}
+const loader = document.getElementById('ctn-loader');
+    async function authentication_token() {
+        const res = await fetch('https://comparame-api.herokuapp.com/user/authenticate', {
+            method: 'GET',
+            headers: {
+                'Authorization': user_token
+            }
+        })
+        const response = await res.json()
+        respuesta_api = response
+    }
+    authentication_token()
+    let respuesta_api 
 // if(typeof(user_token)=="string"){
 //     const validar= autentication(user_token);
 //     if(validar.status){
-//     }
-// }
-
-(async()=>{
-    if(user_token){
-        try{
-            const authenticate = await authentication_token(user_token);
-            if(!authenticate.status){
-                window.location.replace('./index.html');
-            }
+    //     }
+    // }
+    //proteger rutar
+    setTimeout(()=>{
+        if(respuesta_api.status){
+            loader.style.display = "none"
+        }else{
+            window.location.replace('./index.html')
         }
-        catch(err){
-            window.location.replace('./index.html');
-       }
-    }else{
-        window.location.replace('./index.html')
-    }
-})();
-if(typeof(user_token)=="object"){
+    },9000)
+// (async () => {
+//     if (respuesta_api.status){
+//         try {
+//             const authenticate = await authentication_token();
+//             if (authenticate.status) {
+//                 window.location.replace('./miLista.html')
+//                 loader.style.display = "none"
+//             }
+//             if (!authenticate.status) {
+//                 window.location.replace('./index.html');
+//             }
+//         }
+//         catch (err) {
+//             window.location.replace('./index.html');
+//         }
+//     }
+// })();
+if (user_token == null) {
     window.location.replace('./index.html')
-}else{
-    window.location.replace('./index.html')
-}
-//proteger rutar
+} 
+// else {
+//     window.location.replace('./index.html')
+// }
